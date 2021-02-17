b966eb0d5f3bf24cf5351b58dc9ad78cd86e8cf3
Naive96 <uo245494@uniovi.es>
2021-02-18 00:35:57 +0100

    fix mysqlcon

--- AppSW/src/persistence/MySQLCon.java
+++ AppSW/src/persistence/MySQLCon.java
@@ -7,24 +7,19 @@ public class MySQLCon {
 
 	private Connection con;
 
+
 	public Connection getConnection() {
 		if (con == null) {
 			try {
 				Class.forName("com.mysql.jdbc.Driver");
 				this.con = DriverManager.getConnection("jdbc:mysql://localhost:3306/prueba_sw?user=vmchaves&password=123456&useSSL=false");
-				System.out.println(this.con.toString());
-				Statement stm = con.createStatement();
-				ResultSet rs = stm.executeQuery("select * from user");
-				while (rs.next())
-					System.out.println(rs.getString(1));
 
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 		return con;
-	}
-	
+	}	
 	public void closeConnection() {
 		try {
 			con.close();
