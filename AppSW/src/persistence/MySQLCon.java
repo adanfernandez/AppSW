e4c1713f329061fa851ab10984457edcbcc8ffea
Naive96 <uo245494@uniovi.es>
2021-02-18 20:55:53 +0100

    added configuration and .properties files

--- AppSW/src/persistence/MySQLCon.java
+++ AppSW/src/persistence/MySQLCon.java
@@ -2,24 +2,34 @@ package persistence;
 
 import java.sql.*;
 
+import conf.Config;
 
 public class MySQLCon {
 
 	private Connection con;
-
+	private Config config = Config.getInstance();
 
 	public Connection getConnection() {
 		if (con == null) {
 			try {
-				Class.forName("com.mysql.jdbc.Driver");
-				this.con = DriverManager.getConnection("jdbc:mysql://localhost:3306/prueba_sw?user=vmchaves&password=123456&useSSL=false");
+				Class.forName(config.get("DRIVER"));
+				this.con = DriverManager
+						.getConnection(config.get("JDBC") + config.get("HOST") + "/" + config.get("DATABASE") + "?user="
+								+ config.get("USERNAME") + "&password=" + config.get("PASSWORD") + "&allowPublicKeyRetrieval=true&useSSL=false");
 
+//				TODO: Delete
+//				Statement st = con.createStatement();
+//				ResultSet rs = st.executeQuery("SELECT email FROM user");
+//				while(rs.next()) {
+//					System.out.println(rs.getString(1));
+//				}
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 		return con;
-	}	
+	}
+
 	public void closeConnection() {
 		try {
 			con.close();
@@ -29,10 +39,3 @@ public class MySQLCon {
 	}
 
 }
-
-//// here sonoo is database name, root is username and password
-//Statement stmt = con.createStatement();
-//ResultSet rs = stmt.executeQuery("select * from emp");
-//while (rs.next())
-//	System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
-//con.close();
