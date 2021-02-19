1d2a47d9f029900be812d6b8b593a1ea63bf9e4d
Adán Fernández <47212287+adanvetusta@users.noreply.github.com>
2021-02-19 23:26:16 +0100

    Methods from business layer for Panel and User

--- AppSW/src/business/panelmanager/PanelManager.java
+++ AppSW/src/business/panelmanager/PanelManager.java
@@ -1,5 +1,21 @@
 package business.panelmanager;
 
+import java.util.List;
+
+import model.Panel;
+
 public class PanelManager implements PanelManagerService {
+
+	@Override
+	public List<Panel> getPanelsFromUser(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deletePanel(Long id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
--- AppSW/src/business/panelmanager/PanelManagerService.java
+++ AppSW/src/business/panelmanager/PanelManagerService.java
@@ -1,5 +1,13 @@
 package business.panelmanager;
 
-public interface PanelManagerService {
+import java.util.List;
 
+import model.Panel;
+
+public interface PanelManagerService {
+	
+	public List<Panel> getPanelsFromUser(Long id);
+	
+	public boolean deletePanel(Long id);
+	
 }
