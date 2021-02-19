05110077d714683c753b855dba819337e37f0e59
Adán Fernández <47212287+adanvetusta@users.noreply.github.com>
2021-02-19 23:03:22 +0100

    Script DB - creation

---
+++ AppSW/database/db_creation.sql
@@ -0,0 +1,167 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 16-02-2021 a las 20:11:17
+-- Versión del servidor: 10.4.17-MariaDB
+-- Versión de PHP: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `prueba_sw`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `panel`
+--
+
+CREATE TABLE `panel` (
+  `name` varchar(255) NOT NULL,
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `deleted` BOOLEAN default false
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `state`
+--
+
+CREATE TABLE `state` (
+  `name` varchar(255) NOT NULL,
+  `order` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `id_panel` int(11) NOT NULL,
+  `deleted` BOOLEAN default false
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `task`
+--
+
+CREATE TABLE `task` (
+  `title` varchar(255) NOT NULL,
+  `location` int(11) DEFAULT NULL,
+  `expirationDate` datetime DEFAULT NULL,
+  `descripcion` varchar(512) DEFAULT NULL,
+  `id` int(11) NOT NULL,
+  `id_state` int(11) NOT NULL,
+  `deleted` BOOLEAN
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `email` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `surname` varchar(255) NOT NULL,
+  `phone` varchar(12) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `Id` int(11) NOT NULL,
+  `deleted` BOOLEAN default false
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `panel`
+--
+ALTER TABLE `panel`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `state`
+--
+ALTER TABLE `state`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_panel` (`id_panel`);
+
+--
+-- Indices de la tabla `task`
+--
+ALTER TABLE `task`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_state` (`id_state`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `panel`
+--
+ALTER TABLE `panel`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `state`
+--
+ALTER TABLE `state`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `task`
+--
+ALTER TABLE `task`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `panel`
+--
+ALTER TABLE `panel`
+  ADD CONSTRAINT `panel_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `user` (`Id`);
+
+--
+-- Filtros para la tabla `state`
+--
+ALTER TABLE `state`
+  ADD CONSTRAINT `state_ibfk_1` FOREIGN KEY (`id_panel`) REFERENCES `panel` (`id`);
+
+--
+-- Filtros para la tabla `task`
+--
+ALTER TABLE `task`
+  ADD CONSTRAINT `task_ibfk_1` FOREIGN KEY (`id_state`) REFERENCES `state` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
